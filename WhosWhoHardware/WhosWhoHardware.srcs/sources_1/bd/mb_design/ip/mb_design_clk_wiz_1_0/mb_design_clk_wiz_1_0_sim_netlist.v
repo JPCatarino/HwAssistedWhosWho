7072d10a92e39b7f611bdbe0ebaf65f3517c8081
fabioal0 <fabioal0@hotmail.com>
2020-07-02 20:00:53 +0100

    hardware base

--- WhosWhoHardware/WhosWhoHardware.srcs/sources_1/bd/mb_design/ip/mb_design_clk_wiz_1_0/mb_design_clk_wiz_1_0_sim_netlist.v
+++ WhosWhoHardware/WhosWhoHardware.srcs/sources_1/bd/mb_design/ip/mb_design_clk_wiz_1_0/mb_design_clk_wiz_1_0_sim_netlist.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
-// Date        : Thu Mar 26 10:21:54 2020
-// Host        : ASRO-NUC running 64-bit major release  (build 9200)
-// Command     : write_verilog -force -mode funcsim -rename_top mb_design_clk_wiz_1_0 -prefix
-//               mb_design_clk_wiz_1_0_ mb_design_clk_wiz_1_0_sim_netlist.v
+// Date        : Thu Jul  2 19:34:00 2020
+// Host        : DESKTOP-ES9CM6O running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode funcsim
+//               E:/CR_proj/WhosWhoHardware/WhosWhoHardware.srcs/sources_1/bd/mb_design/ip/mb_design_clk_wiz_1_0/mb_design_clk_wiz_1_0_sim_netlist.v
 // Design      : mb_design_clk_wiz_1_0
 // Purpose     : This verilog netlist is a functional simulation representation of the design and should not be modified
 //               or synthesized. This netlist cannot be used for SDF annotated simulation.
@@ -35,6 +35,7 @@ module mb_design_clk_wiz_1_0
         .resetn(resetn));
 endmodule
 
+(* ORIG_REF_NAME = "mb_design_clk_wiz_1_0_clk_wiz" *) 
 module mb_design_clk_wiz_1_0_mb_design_clk_wiz_1_0_clk_wiz
    (clk_out1,
     resetn,
--- WhosWhoHardware/WhosWhoHardware.srcs/sources_1/bd/mb_design/ip/mb_design_clk_wiz_1_0/mb_design_clk_wiz_1_0_stub.v
+++ WhosWhoHardware/WhosWhoHardware.srcs/sources_1/bd/mb_design/ip/mb_design_clk_wiz_1_0/mb_design_clk_wiz_1_0_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
-// Date        : Thu Mar 26 10:21:54 2020
-// Host        : ASRO-NUC running 64-bit major release  (build 9200)
-// Command     : write_verilog -force -mode synth_stub -rename_top mb_design_clk_wiz_1_0 -prefix
-//               mb_design_clk_wiz_1_0_ mb_design_clk_wiz_1_0_stub.v
+// Date        : Thu Jul  2 19:34:00 2020
+// Host        : DESKTOP-ES9CM6O running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               E:/CR_proj/WhosWhoHardware/WhosWhoHardware.srcs/sources_1/bd/mb_design/ip/mb_design_clk_wiz_1_0/mb_design_clk_wiz_1_0_stub.v
 // Design      : mb_design_clk_wiz_1_0
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7a100tcsg324-1
